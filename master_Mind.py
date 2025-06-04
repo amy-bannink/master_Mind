951b4d3d0816d8f5369bc1efbc80d7e2e90f343a
amybannink <amy.bannink@baroes.nl>
2025-06-04 13:02:43 +0200

    Beveiliging

--- master_Mind.py
+++ master_Mind.py
@@ -7,6 +7,8 @@
 print("MasterMind")
 
 import random
+#comment toegevoegd
+
 
 def generate_Code(length=4, digits=6):
     return [str(random.randint(1, digits)) for _ in range(length)]
@@ -41,10 +43,20 @@ def play_Mastermind():
         valid_Guess = False
         while not valid_Guess:
             guess = input(f"Attempt {attempt}: ").strip()
+
+            if guess == "cheat":
+                password = input("Enter cheat password: ").strip()
+                if password == "admin":
+                    show_Secret(secret_Code)
+                else:
+                    print("Incorrect password. Cheat mode denied.")
+                continue  # Terug naar input vragen
+
             valid_Guess = len(guess) == 4 and all(c in "123456" for c in guess)
             if not valid_Guess:
                 print("Invalid input. Enter 4 digits, each from 1 to 6.")
-            show_Secret(secret_Code) if guess == "cheat" else False
+
+
 
         black, white = get_Feedback(secret_Code, guess)
         print(f"Black pegs (correct position): {black}, White pegs (wrong position): {white}")
---
+++ wiebenjij.py
@@ -0,0 +1,4 @@
+merk_telefoon = input("Wat is het merk van je telefoon?")
+merk_laptop = input("Wat is het merk van je laptop?")
+leeftijd = input("Wat is je leeftijd?")
+favo_vak = input("Wat is je favoriete vak?")
